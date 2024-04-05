56cacce146818c79c158512696e443b21d251959
Sourabh Maurya <shmaurya2014.com>
2024-04-06 00:31:40 +0530

    day4 learn how to make app layout and  write the layout component sepretaly part 1

--- App.js
+++ App.js
@@ -1,46 +1,70 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
 
-// Manipulate the HTML DOM using React
-
-// Create nested React Elements
 
 import React from "react";
 import ReactDOM  from "react-dom/client";
-// const heading = React.createElement(
-//     "h1",
-//     {
-//       id: "heading",
-//      },
-//     "React is superpower"
-//   );
 
+const Header=()=>{
+  return ( 
+  <div className="header">
+    <div className="logo-container">
+     <img className="logo" src="https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg"/> </div>
   
-   //JSX =>React.createElement =>ReactElement-JS Object =>HTMLElement(render)
-   //react element  
-     // babel help to conver jsx to react.createelement
-  
-  //React Functional Component
-  const HeadingComponent1=()=>{
-    return <h1 className="heading1">Namaste React Functional Component </h1>;
-  };
-  const jsxheading=
-  (<h1 className="head">
-    <HeadingComponent1/>
-    React is superpower</h1>); 
-  
-  
-  const HeadingComponent2=()=>(
-      <div id="container">   
-        {jsxheading}      
-      <HeadingComponent1/>        
-      <h1 className="heading2">Namaste React Functional Component 2 </h1>;
+  <div className="nav-items">
+    <ul>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Contact Us</li>
+      <li>Cart</li>
+    </ul>
+  </div>
+  </div>
+)};
+
+const RestaurantCard=() =>{
+  return (
+    <div className="res-cards">
+       <img className="res-logo" 
+      alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rwks2yva4dbppsekgw6a"/>
+      <h1>Bikanervala</h1>
+      <h3>North Indian</h3>
+      <h4>4.5 stars</h4>
+      <h5>25 minutes</h5>
     </div>
-  );
-  // create root using createRoot
+  )
+};
+
+const Body=()=>{
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+       <RestaurantCard/>
+      </div>
+    </div>
+  )
+}
+const AppLayout=()=>{
+  return ( <div className="app">
+    <Header/>
+    <Body/>
+  </div>
+)};
+
+
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(<HeadingComponent2/>);
+  root.render(<AppLayout/>);
--- readme.md
+++ readme.md
@@ -1,4 +1,21 @@
-Day 2 Learn
-how to use one functional component to another functional component 
-<functionalcomponent/>
-2nd functional component never used to the previous functional component it gives throw reference error because end of the day everything is javascript and we cannot use const  value before initialisation
+<!-- Day 3 Learn parcel is beast -->
+
+<!-- how to use one functional component to another functional component 
+<functionalcomponent/>  or <functionalcomponent> <functionalcomponent/> or {functionalcomponent()} -->
+
+<!-- how to use react element in  functional component  
+ {reactelement} -->
+
+<!-- 2nd functional component never used to the previous functional component it gives throw reference error because end of the day everything is javascript and we cannot use const  value before initialisation -->
+
+<!-- * if any malcious data come in my functional component , so jsx do automatically sanitisation 
+const data=api.getdata();   // malcious data 
+
+ const HeadingComponent2=()=>(
+      <div id="container">   
+        {data}     
+      <HeadingComponent1/>        
+      <h1 className="heading2">Namaste React Functional Component 2 </h1>;
+    </div>
+  ); -->
+
