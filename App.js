ed98707cc99850fa718c5c6f5e68da23c4484536
Sourabh Maurya <shmaurya2014.com>
2024-04-05 22:27:37 +0530

    day 3 learn functional component, functional composition etc.

--- App.js
+++ App.js
@@ -10,20 +10,37 @@
 
 import React from "react";
 import ReactDOM  from "react-dom/client";
-const heading = React.createElement(
-    "h1",
-    {
-      id: "heading",
-     },
-    "React is superpower"
-  );
+// const heading = React.createElement(
+//     "h1",
+//     {
+//       id: "heading",
+//      },
+//     "React is superpower"
+//   );
 
-  console.log(heading);
-
-  const jsxheading=<h1>React is superpower</h1>;
-  console.log(jsxheading);
   
+   //JSX =>React.createElement =>ReactElement-JS Object =>HTMLElement(render)
+   //react element  
+     // babel help to conver jsx to react.createelement
+  
+  //React Functional Component
+  const HeadingComponent1=()=>{
+    return <h1 className="heading1">Namaste React Functional Component </h1>;
+  };
+  const jsxheading=
+  (<h1 className="head">
+    <HeadingComponent1/>
+    React is superpower</h1>); 
+  
+  
+  const HeadingComponent2=()=>(
+      <div id="container">   
+        {jsxheading}      
+      <HeadingComponent1/>        
+      <h1 className="heading2">Namaste React Functional Component 2 </h1>;
+    </div>
+  );
   // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(heading);
+  root.render(<HeadingComponent2/>);
---
+++ readme.md
@@ -0,0 +1,4 @@
+Day 2 Learn
+how to use one functional component to another functional component 
+<functionalcomponent/>
+2nd functional component never used to the previous functional component it gives throw reference error because end of the day everything is javascript and we cannot use const  value before initialisation
