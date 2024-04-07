9b731f01be660084e81bf9bd1e737e123cd9eeeb
Sourabh Maurya <shmaurya2014.com>
2024-04-08 00:07:45 +0530

    day 6 part 1 live swiggy api fetch and generate live restaurant cards and learn useEffect hooke concept

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,30 @@
  import RestaurantCard from "./RestaurantCard";
  import resList from "../utility/mockdata";
- import { useState } from "react";
+ import { useEffect, useState } from "react";
  import resList from "../utility/mockdata";
  
  
  const Body=()=>{
     const[listOfRestaurant,setListOfRestaurant]=useState(resList);
+     
+    useEffect(()=>{
+     fetchData();
+    },[]);
+    
+   
 
+    const fetchData= async ()=>{
+        const data= await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        );
+       
+         const json=await data.json();
+
+         console.log(json);
+         //optional chaining
+        setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        };
+       
         return (
             <div className="body">
               <div className="filter">
