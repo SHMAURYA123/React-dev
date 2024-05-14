e41a535e81e37b188f0aa944ea214946cddea4d4
Sourabh Maurya <shmaurya2014.com>
2024-05-14 23:41:23 +0530

    learn useRouteError for more information about error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 
 
@@ -22,10 +24,15 @@ const appRouter=createBrowserRouter([
   {
     path:"/",
     element:<AppLayout/>,
+    errorElement:<Error/>,
   },
   {
     path:"/about",
     element:<About/>,
+  },
+  {
+    path:"/contact",
+    element:<Contact/>,
   }
 ])
 
