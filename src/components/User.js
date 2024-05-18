a3094c882ecedcabb688733d6d6985fb0f92405b
Sourabh Maurya <shmaurya2014.com>
2024-05-18 23:35:07 +0530

    day 8 learn diffrence beteeen class based component and function based component, and also learn how props used in class based component part 1 '

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User=({name})=>{
+    return (
+        <div className="user-card">
+         <h2>Name:{name}</h2>
+         <h3>Location:Delhi</h3>
+         <h4> Contact:@shmaurya123</h4>
+        </div>
+    );
+};
+export default User;
